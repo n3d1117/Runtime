d0c1f3aa560fd4fa5932062124d2c6a53aa4cf0d
ned <github@edoardo.fyi>
2025-02-01 10:54:21 +0100

    Initial Commit

---
+++ Runtime/RuntimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RuntimeApp.swift
+//  Runtime
+//
+//  Created by ned on 01/02/25.
+//
+
+import SwiftUI
+
+@main
+struct RuntimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
